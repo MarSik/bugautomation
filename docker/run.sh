7d9bfef2c56e8714a8c60d01ce76a1d39343ce24
Martin Sivak <msivak@redhat.com>
2017-09-11 12:51:32 +0200

    Add dockerfile and circleci build steps

---
+++ docker/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+/opt/prometheus/prometheus*/prometheus -config.file=/opt/bugautomation/prometheus.yml -storage.local.path=/var/lib/prometheus &
+PROMETHEUS=$!
+
+pushd /opt/grafana/grafana*
+GF_PATHS_DATA=/var/lib/grafana ./bin/grafana-server web &
+GRAFANA=$!
+popd
+
+java -Dbug.config=/etc/bugautomation/config.properties -jar /opt/bugautomation/server-*/*.jar &
+BA=$!
+
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$BA" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+wait $BA
+
+kill $GRAFANA
+kill $PROMETHEUS
+
+wait $GRAFANA
+wait $PROMETHEUS
+
